bff1db7cb78a9bc12dcf976b50634927359fa664
Hoang Hai Nguyen <h_nguyen95@u.pacific.edu>
2021-10-18 22:14:35 +0700

    Fix SM

--- remix-main/main.sol
+++ remix-main/main.sol
@@ -1,7 +1,17 @@
 //SPDX-License-Identifier: open-source
-pragma solidity ^0.8.5;
+pragma solidity ^0.8.2;
+import "./Certificate_NFT.sol";
 
 contract quanly{
+    
+    CertificateNFT NFT;
+    event issueCert(string title, string signature, string content ,uint tokenID);
+   
+    modifier onlyOwner {
+        require(msg.sender == deployAddress, "Ownable: You are not the owner");
+        _;
+    }
+    
     //cấu trúc một giấy chứng nhận
     struct Certificate{
         string title; //tên chứng nhận
@@ -9,6 +19,7 @@ contract quanly{
         uint date;//ngày cấp
         address author;//địa chỉ người cấp
         string signature;//chữ ký sô của người cấp
+        
     }
     //cấu trúc một người có quyền cấp
     struct Author{
@@ -17,21 +28,19 @@ contract quanly{
     }
     
     address public deployAddress;//biến lưu địa chỉ của quản trị viên tối cao
-    constructor(){
+    constructor(address _AddressNFT){
         deployAddress = msg.sender;//gán đại chỉ quản trị viên bằng người deploy hợp đồng thông minh
+        NFT = CertificateNFT(_AddressNFT);
     }
+    
     mapping (address => Author) public authors;//chứa dữ liệu những người có quyền cấp chứng nhận
     mapping(address => Certificate[]) public student; //chứa dữ liệu người được nhận chứng nhận
     
     
     //hàm cấp quyền cho author chỉ admin tối cao mới có quyền chạy hàm
-    function addAuthor(string memory name, address _address) public{
-        if(msg.sender == deployAddress){
-            authors[_address].status = true;
-            authors[_address].name = name;
-        }else{
-            revert("khong cos quyen");
-        }
+    function addAuthor(string memory name, address _address) public onlyOwner{
+        authors[_address].status = true;
+        authors[_address].name = name;
     } 
     
     
@@ -40,6 +49,8 @@ contract quanly{
         
         if(authors[msg.sender].status){
             student[_student].push(Certificate(title,content,block.timestamp,msg.sender,signature));
+            uint index = NFT.safeMint(msg.sender,content);
+            emit issueCert(title, signature, content , index);
         }else revert("khong cos quyen");
         
     }
